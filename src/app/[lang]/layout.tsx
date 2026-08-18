ee7306506aa302b284dcbf50028614de95492bfd
belkakakashka2012-a11y <belkakakashka2012@gmail.com>
2026-08-18 21:28:37 +0400

    Import Inter font in layout component

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,4 +1,6 @@
 import type { Metadata } from "next";
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
 import "./globals.css";
 import { ConfigsProvider } from "@/lib/providers/ConfigsProvider";
 import { getCookiesArray } from "@/lib/actions/cookies";
